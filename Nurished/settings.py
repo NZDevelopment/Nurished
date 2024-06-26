00e8dd8088610422ac66d5d1de4aa193ec505975
Nidhal Zarrad <nidhalz@gmail.com>
2024-06-26 17:02:14 +0000

    Deployment Commit

--- Nurished/settings.py
+++ Nurished/settings.py
@@ -12,20 +12,32 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+#NZ
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR,
+'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f70!4tr-^r7ko6*o_ager-3tql7rb)ce@vg1taf+hppl@%^yj@'
+#SECRET_KEY = 'django-insecure-f70!4tr-^r7ko6*o_ager-3tql7rb)ce@vg1taf+hppl@%^yj@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-nzdevelopment-nurished-h57wydwe4eb.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-nzdevelopment-nurished-h57wydwe4eb.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -36,11 +48,17 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    #'django.contrib.staticfiles',
+    'nurishedApp',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,14 +72,14 @@ ROOT_URLCONF = 'Nurished.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages',                
             ],
         },
     },
@@ -73,12 +91,23 @@ WSGI_APPLICATION = 'Nurished.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+'default':
+dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+#DATABASE_URL = os.getenv('DATABASE_URL')
+#DATABASES = {
+#    'default': dj_database_url.config(),
+#}
+
+
 
 
 # Password validation
@@ -116,6 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,
+'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
